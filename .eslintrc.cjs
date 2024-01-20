5d07055f416150176e8c4f7dc77d6b236d70369d
isakiDev <gaspar.c.developer@gmail.com>
2024-01-19 22:03:20 -0300

    feat(auth): remove sweetalert and add sonner

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,8 @@ module.exports = {
     ],
     "rules": {
         "@typescript-eslint/explicit-function-return-type": "off",
-        "react/react-in-jsx-scope": "off"
+        "react/react-in-jsx-scope": "off",
+
+        "@typescript-eslint/no-floating-promises": "off"
     }
 }
